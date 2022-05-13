928d37fdc0abea24ab25b2b21715c597b1204665
Cristian Orrego Dev <cristian.duquew@gmail.com>
2022-05-13 18:25:05 -0500

    Agrega controlador Register y ruta Register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/register', function () {
-    return view('auth.register');
-});
+Route::get('/register', [RegisterController::class, 'index']);
